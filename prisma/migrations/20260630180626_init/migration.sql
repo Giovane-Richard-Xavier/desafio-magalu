6ce0c0866f19c5c7d12f2bc13c01165cf4c7112b
Giovane Richard <giovane@rlvtecnologia.com.br>
2026-06-30 15:09:18 -0300

    feat: implement init migrations

---
+++ prisma/migrations/20260630180626_init/migration.sql
@@ -0,0 +1,39 @@
+-- CreateEnum
+CREATE TYPE "EnumChanel" AS ENUM ('EMAIL', 'SMS', 'PUSH', 'WHATSAPP');
+
+-- CreateEnum
+CREATE TYPE "EnumStatus" AS ENUM ('PENDING', 'SUCCESS', 'CANCELED', 'ERROR');
+
+-- CreateTable
+CREATE TABLE "Notification" (
+    "id" TEXT NOT NULL,
+    "dateTime" TIMESTAMP(3) NOT NULL,
+    "destination" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "channelId" INTEGER NOT NULL,
+    "statusId" INTEGER NOT NULL,
+
+    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Channel" (
+    "id" SERIAL NOT NULL,
+    "description" "EnumChanel" NOT NULL DEFAULT 'EMAIL',
+
+    CONSTRAINT "Channel_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Status" (
+    "id" SERIAL NOT NULL,
+    "description" "EnumStatus" NOT NULL DEFAULT 'PENDING',
+
+    CONSTRAINT "Status_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_channelId_fkey" FOREIGN KEY ("channelId") REFERENCES "Channel"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Notification" ADD CONSTRAINT "Notification_statusId_fkey" FOREIGN KEY ("statusId") REFERENCES "Status"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
